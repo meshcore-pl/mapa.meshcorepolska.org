24ffa0b31fd8a11c6849c3c2f59c8dc4ffa8837b
Sefinek <contact@sefinek.net>
2026-07-20 01:44:26 +0200

    0.2.0

--- public/js/changelog.js
+++ public/js/changelog.js
@@ -2,7 +2,23 @@ import { initModal } from './modal.js';
 
 const CHANGELOG = [
 	{
+		version: '0.2.0',
+		date: '20.07.2026',
+		changes: [
+			'Dodano ustawienie "Zamykaj okno filtrów po zastosowaniu".',
+			'Dodano ustawienie "Pokaż OpenFreeMap w przełączniku map".',
+			'Dodano nowe mapy: CartoDB Dark, CartoDB Positron, CyclOSM oraz Humanitarian OSM.',
+			'CartoDB Dark od teraz jest domyślną mapą.',
+			'Dodano listę zmian.',
+			'Poprawiono i ulepszono funkcjonalność filtrów.',
+			'Poprawiono niektóre animacje.',
+			'Przebudowano przycisk "i" w prawym dolnym rogu strony.',
+			'Inne drobne poprawki w wyglądzie.',
+		],
+	},
+	{
 		version: '0.1.0',
+		date: '19.07.2026',
 		changes: [
 			'Polskojęzyczny interfejs.',
 			'Możliwość przełączania między węzłami z Polski i całego świata.',
@@ -22,7 +38,10 @@ export const initChangelogModal = ({ escapeHtml }) => {
 
 	listEl.innerHTML = CHANGELOG.map(entry => `
 		<li class="changelog-version">
-			<div class="changelog-version-title">v${escapeHtml(entry.version)}</div>
+			<div class="changelog-version-title">
+				<span>v${escapeHtml(entry.version)}</span>
+				<time class="changelog-version-date">${escapeHtml(entry.date)}</time>
+			</div>
 			<ul class="changelog-changes">
 				${entry.changes.map(change => `<li>${escapeHtml(change)}</li>`).join('')}
 			</ul>
